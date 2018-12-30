2883d6efd8670825f842eeff5d4de217a8734ef9
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-12-31 03:52:09 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content">
+      <div class="one">
+        <textarea v-model="input"></textarea>
+      </div>
+      <div class="two"></div>
+    </div>
+    <!--<img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// eslint-disable-next-line
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  // eslint-disable-next-line
+  name: "home",
   components: {
-    HelloWorld,
+    // eslint-disable-next-line
+    //HelloWorld
+    // eslint-disable-next-line
   },
+  data() {
+    return {
+      input: ""
+    };
+  }
 };
 </script>
+<style scoped lang='scss'>
+@import "../../public/styles.scss";
+
+.content {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2px;
+}
+.one,
+.two {
+  border: 5px solid orangered;
+  height: 600px;
+}
+textarea {
+  width: 100%;
+  height: 100%;
+}
+</style>
