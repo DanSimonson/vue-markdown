bb00035395728e88609fb65247cf45c7deee1d14
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-12-31 18:28:52 +0800

    first draft

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 
 <template>
   <div class="home">
-    <button @click="clearText">Click here to clear the Editor</button>
-    <div class="content">
-      <div class="one">
-        <textarea v-model="input"></textarea>
-      </div>
-      <div class="two" id="preview" v-html="viewMarkdown"></div>
+    <div class="center">
+      <button @click="clearText" class="button">Click here to clear the Editor</button>
     </div>
-    <!--<img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div class="content">
+      <textarea v-model="input" id="editor"></textarea>
+      <div id="preview" v-html="viewMarkdown"></div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +15,7 @@
 // @ is an alias to /src
 // eslint-disable-next-line
 //import HelloWorld from "@/components/HelloWorld.vue";
+import url from "../assets/logo.png";
 
 export default {
   // eslint-disable-next-line
@@ -71,12 +70,18 @@ And here. | Okay. | I think we get it.
 - Even if you use dashes or asterisks.
 * And last but not least, let's not forget embedded images:
 
-![Benjamin Bannekat](https://octodex.github.com/images/bannekat.png)`
+![Vue.js Image](${url})`
     };
   },
   computed: {
     viewMarkdown() {
-      return marked(this.input, { breaks: true, anchorTargetBlank: true });
+      return marked(this.input);
+      /*![Vue.js Image](${url})`{ breaks: true, anchorTargetBlank: true } */
+    }
+  },
+  methods: {
+    clearText() {
+      return (this.input = "");
     }
   }
 };
@@ -91,14 +96,110 @@ And here. | Okay. | I think we get it.
   max-width: 960px;
   grid-gap: 2px;
 }
-.one,
-.two {
-  border: 5px solid orangered;
-}
+
 textarea {
   width: 100%;
   height: 100%;
+  border: 2px solid $Grey;
 }
 #preview {
+  border: 2px solid $Grey;
+}
+.button::-moz-focus-inner {
+  border: 0;
+  padding: 0;
+  border: 1px solid #Light-gray;
+}
+.center {
+  margin: 50px auto;
+  text-align: center;
+}
+
+.button {
+  display: inline-block;
+  *display: inline;
+  zoom: 1;
+  padding: 6px 20px;
+  margin: 0;
+  cursor: pointer;
+  border: 1px solid #bbb;
+  overflow: visible;
+  font: bold 13px arial, helvetica, sans-serif;
+  text-decoration: none;
+  white-space: nowrap;
+  color: #555;
+
+  background-color: #ddd;
+  background-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(rgba(255, 255, 255, 1)),
+    to(rgba(255, 255, 255, 0))
+  );
+  background-image: -webkit-linear-gradient(
+    top,
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 0)
+  );
+  background-image: -moz-linear-gradient(
+    top,
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 0)
+  );
+  background-image: -ms-linear-gradient(
+    top,
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 0)
+  );
+  background-image: -o-linear-gradient(
+    top,
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 0)
+  );
+  background-image: linear-gradient(
+    top,
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 0)
+  );
+
+  -webkit-transition: background-color 0.2s ease-out;
+  -moz-transition: background-color 0.2s ease-out;
+  -ms-transition: background-color 0.2s ease-out;
+  -o-transition: background-color 0.2s ease-out;
+  transition: background-color 0.2s ease-out;
+  background-clip: padding-box; /* Fix bleeding */
+  -moz-border-radius: 3px;
+  -webkit-border-radius: 3px;
+  border-radius: 3px;
+  -moz-box-shadow: 0 1px 0 rgba(0, 0, 0, 0.3), 0 2px 2px -1px rgba(0, 0, 0, 0.5),
+    0 1px 0 rgba(255, 255, 255, 0.3) inset;
+  -webkit-box-shadow: 0 1px 0 rgba(0, 0, 0, 0.3),
+    0 2px 2px -1px rgba(0, 0, 0, 0.5), 0 1px 0 rgba(255, 255, 255, 0.3) inset;
+  box-shadow: 0 1px 0 rgba(0, 0, 0, 0.3), 0 2px 2px -1px rgba(0, 0, 0, 0.5),
+    0 1px 0 rgba(255, 255, 255, 0.3) inset;
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.9);
+
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.button:hover {
+  background-color: #eee;
+  color: #555;
+}
+
+.button:active {
+  background: #e9e9e9;
+  position: relative;
+  top: 1px;
+  text-shadow: none;
+  -moz-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3) inset;
+  -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3) inset;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.3) inset;
 }
 </style>
